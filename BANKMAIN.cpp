11d1474bdc09790eb150a2891d2cef482f1fdff1
sameh.khalil <samehkh35@gmail.com>
2022-09-14 20:21:36 +0200

    yummi sphagittis

---
+++ BANKMAIN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+
+
+
+int main (void)
+{
+	return 0 ;
+}
---
+++ cusDB.h
@@ -0,0 +1,37 @@
+
+#ifndef __CUS_DB_H__
+
+#define	__CUS_DB_H__
+
+
+#include <string.h>
+#include "STD_TYPES.h"
+
+
+typedef struct{
+	char * _BID;
+	char ** _N;
+	char * _P;
+	char * _S;
+	char * _BAL;
+	char * _nID;
+	char * _ADDR;
+	char * _AGE;	
+	char * _GnID;
+	u32 strsize ;
+	u8 state ;
+}cus_t;
+
+
+
+cus_t cussearch ( char ** name , char * password);
+void fhm(cus_t * s_cus);
+void insertEditedcus (cus_t * s_new_cus);
+void insertcusAtend (cus_t * s_new_cus);
+void GETBID (cus_t * s_new_cus);
+u32 strtoint (char * c);
+std::string inttostr (u32 num);
+cus_t getcusbyBID (char * BID);
+
+
+#endif
